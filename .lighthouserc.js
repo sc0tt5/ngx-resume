31cf9f08bcec36bc59955972c25048fd628a72a3
Scott Robertson <contact@ScottRobertson.io>
2024-03-30 17:25:07 -0500

    issue-86 adjust lh config

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -13,11 +13,11 @@ module.exports = {
         'largest-contentful-paint': ['warn', { maxNumericValue: 1200 }],
         'speed-index': ['warn', { maxNumericValue: 1000 }],
         // application not setup for production server
-        'bf-cache': 'off', // no forward or backwards navigation
         'csp-xss': 'off',
+        'target-size': 'off', // designed for print
+        'total-byte-weight': 'off', // borderline exceeds 5k KiB; optimize later
         'unused-javascript': 'off', // remove from main bundle if possible
-        'uses-text-compression': 'off',
-        'unminified-javascript': 'off' // false positive?
+        'uses-text-compression': 'off' // if using actual server
       }
     },
     collect: {
